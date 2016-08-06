50dbb8037637996eb3aa34f865d4df1e9b11c3d2
Lauri Peltonen <lauri.peltonen@iki.fi>
2016-08-07 01:14:50 +0300

    Propagate updated gif location to redux state

--- src/reducers/scenes.js
+++ src/reducers/scenes.js
@@ -1,10 +1,37 @@
-const initialState = [];
 
-export default function scenes(state = initialState, action) {
+function gif(state, action) {
+  switch (action.type) {
+    case 'MOVE_DANCER':
+      return {
+        position: { left: action.x, bottom: action.y },
+        height: action.height,
+      };
+    default:
+      return state;
+  }
+}
+
+function scene(state, action) {
+  switch (action.type) {
+    case 'MOVE_DANCER':
+      return {
+          ...state,
+          gifs: [
+              ...state.gifs.slice(0, action.id),
+              gif(state.gifs[action.id], action),
+              ...state.gifs.slice(action.id + 1)
+          ]
+      };
+    default:
+      return state;
+  }
+}
+
+export default function scenes(state = [], action) {
   switch (action.type) {
     case 'MOVE_DANCER':
       console.log('dragDancer action', action);
-      return state;
+      return state.map(s => scene(s, action));
     default:
       return state;
   }
