6256aa0dd29ab089552ce6b6cb3183901ebb5db2
Lauri Peltonen <lauri.peltonen@iki.fi>
2016-08-07 22:31:23 +0300

    Refactor Dancer.js with redux state instead of react state

--- client/src/Dancer.js
+++ client/src/Dancer.js
@@ -1,39 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ResizableAndMovable from 'react-resizable-and-movable';
 import './Dancer.css';
 
-export default class Dancer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { x: props.x, y: props.y, height: props.height, width: 100 };
-  }
-  onResizeStop(direction, styleSize, clientSize, delta) {
-      this.setState({ height: styleSize.height });
-      if (direction === 'top')
-        this.setState({ y: this.state.y - delta.height});
-      this.props.onDragged(this.state.x, this.state.y, styleSize.height);
-  }
-  onDragStop(event, ui) {
-      this.setState({ x: ui.position.left + this.state.width / 2, y: ui.position.top });
-      this.props.onDragged(this.state.x, this.state.y, this.state.height);
-  }
-  render() {
-    return (
-      <ResizableAndMovable
-        x={this.state.x - this.state.width / 2}
-        y={this.state.y}
-        height={this.state.height}
-        width={this.state.width}
-        onResizeStop={this.onResizeStop.bind(this)}
-        onDragStop={this.onDragStop.bind(this)}
-        minWidth={100}
-        minHeight={100}
-        isResizable={{ top:true, right:false, bottom:true, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false }}
-        bounds={'parent'}
-        className="dancer"
-     >
-        <img src={this.props.src} alt="dancer" />
-     </ResizableAndMovable>
-    );
-  }
-}
+const Dancer = ({src, x, y, height, onDragged }) => {
+  const width = 100;
+  return (
+    <ResizableAndMovable
+      x={x - width / 2}
+      y={y}
+      height={height}
+      width={width}
+      onResizeStop={(direction, styleSize, clientSize, delta) => {
+        onDragged(x, direction === 'top' ? y - delta.height : y, styleSize.height);
+      }}
+      onDragStop={(event, ui) => {
+        onDragged(ui.position.left + width / 2, ui.position.top, height);
+      }}
+      minWidth={100}
+      minHeight={100}
+      isResizable={{ top:true, right:false, bottom:true, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false }}
+      bounds={'parent'}
+      className="dancer"
+    >
+      <img src={src} alt="dancer" />
+    </ResizableAndMovable>
+  );
+};
+
+export default Dancer;
